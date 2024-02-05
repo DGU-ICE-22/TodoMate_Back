944deea6fd11b9f1e806bfead61b02a598e54867
5hseok <ohhs1010@gmail.com>
2024-02-05 15:56:11 +0900

    ✨ Feat : 회원가입 View 작성 post로 atomic 사용해서 유저 save 후 토큰 만들어서 Response로 응답

--- user/views.py
+++ user/views.py
@@ -4,10 +4,35 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404, RetrieveUpdateDestroyAPIView
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, UserLoginSerializer
 from .models import User
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from knox.models import AuthToken
 # Create your views here.
+from django.db import transaction
+
 class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegisterSerializer
-        
+
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception = True)
+        user = serializer.save()
+        _, token = AuthToken.objects.create(user)
+        return Response({
+            "user": UserRegisterSerializer(user, context=self.get_serializer_context()).data,
+            "token": token
+        })
+
+
+class UserLoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+    
+    def post(self, request):
+        serializer = self.get_serializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        token = serializer.validated_data
+        return Response({"token":token.key}, status=status.HTTP_200_OK)
