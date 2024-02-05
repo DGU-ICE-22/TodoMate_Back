279e4c3ee75928d2935f4befd59363e85bc23ce4
5hseok <ohhs1010@gmail.com>
2024-02-05 15:35:38 +0900

    ✨ Feat : 회원가입 url 등록

--- Todomate/urls.py
+++ Todomate/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("user.urls", "todo.urls")),
+    path("api/user/", include("user.urls")),
+    # path("api/todo/",include("todo.urls")),
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import UserRegisterView, UserLoginView
+from .views import UserRegisterView, UserLoginView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view()),
