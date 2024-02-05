e8095fa02b6dd5d79f3ba359bc5d59e9d6543dda
5hseok <ohhs1010@gmail.com>
2024-02-05 16:31:16 +0900

    ♻️ Refactor : SECRET_KEY 환경변수 숨김 및 DEBUG 설정 끄기

--- Todomate/settings.py
+++ Todomate/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url, environ
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0^!g66!sgj%f)1*a*7ea$ai)51sql3$krg(^wkbgi82*t-l!dz"
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
