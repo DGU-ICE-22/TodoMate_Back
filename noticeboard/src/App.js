419fdc6196403e7bc74360248c12e5ef70f84f49
5hseok <ohhs1010@gmail.com>
2024-02-12 18:46:39 +0900

    Feat : profile 라우터 추가

--- noticeboard/src/App.js
+++ noticeboard/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import Register from './pages/register';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <Route path="/" element={<MainPage />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
     );
 }
