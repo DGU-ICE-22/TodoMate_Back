b7258e3c9b0468c51d315ba98d1e46406dfb9546
5hseok <ohhs1010@gmail.com>
2024-02-13 23:45:07 +0900

    Feat : ProfileView 생성 및 get 메소드 추가 User 모델 불러오기

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-from user.serializers import UserRegisterSerializer, UserLoginSerializer
 from .serializers import ProfileSerializer
-from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics, status
 from .models import Profile
+
+User = get_user_model()
 class ProfileView(generics.RetrieveUpdateAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+    def get(self, request, nickname):  # 수정한 부분
+        user = get_object_or_404(User, nickname=nickname)  # 수정한 부분
+        profile = get_object_or_404(Profile, user=user)  # 수정한 부분
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data, status = status.HTTP_200_OK)
