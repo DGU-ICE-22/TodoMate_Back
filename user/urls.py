fc79fadc5a5e783e584ea1c293876fbb461074cc
5hseok <ohhs1010@gmail.com>
2024-02-13 23:33:39 +0900

    Feat : Profile URL 추가

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import UserRegisterView, UserLoginView
+from userprofile.views import ProfileView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view()),
     path('login/', UserLoginView.as_view()),
+    path('profile/', include("userprofile.urls")),
 ]
