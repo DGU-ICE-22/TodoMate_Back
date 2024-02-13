00c5a4d90b3c7a0190f9a114472d22a31952d07e
5hseok <ohhs1010@gmail.com>
2024-02-13 23:42:51 +0900

    Feat : 프로필 시리얼라이저

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Profile
+from user.models import User
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['userid', 'realname', 'nickname', 'phonenumber']
 
 class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Profile
-        fields = {"job", "interest", "profile_image"}
+        fields = ['user', 'job', 'interest', 'profile_image']
